006c51f039c4b52d903cef964ceaf65600b604e6
cherry <cherry@cherry-takuan.org>
2024-05-25 17:48:05 +0900

    トークナイザと再帰下降構文解析をがっちゃんこ

--- nlp-16a/compiler/main.c
+++ nlp-16a/compiler/main.c
@@ -9,116 +9,95 @@
 #define MAX_LINE 1000//最大行数
 #define MAX_ONELINE 85//一行の文字数
 char* src;//コードを格納
+char* src_p;
 
 void error_at(char *loc, char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
 
     int pos = loc - src;
+    fprintf(stderr, "\x1b[31m");
     fprintf(stderr, "%s\n", src);
     fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
     fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
+    fprintf(stderr, "\x1b[39m\n");
     exit(1);
 }
 
 // トークン関連
 // トークンの種類
 typedef enum {
-  TK_RESERVED, // 記号
-  TK_NUM,      // 整数トークン
-  TK_EOF,      // 入力の終わりを表すトークン
+    //0-127は一文字トークン(+や*等)で使用．文字コードそのまま．
+    TK_NUM=128,      // 整数トークン
+    TK_EOF,      // 入力の終わりを表すトークン
 } TokenKind;
 
 typedef struct Token Token;
-
 // トークン型
 struct Token {
-  TokenKind kind; // トークンの型
-  Token *next;    // 次の入力トークン
-  int val;        // kindがTK_NUMの場合、その数値
-  char *str;      // トークン文字列
-  int len;
+    TokenKind kind; // トークンの型
+    int val;        // kindがTK_NUMの場合、その数値
+    char *str;      // トークン文字列
+    int len;
 };
 
-Token *token;
-
-// 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str) {
-    Token *tok = calloc(1, sizeof(Token));
-    tok->kind = kind;
-    tok->str = str;
-    cur->next = tok;
+// 次のトークンが期待している記号のときには、トークンを1つ読み進めてトークンを返す。それ以外の場合には偽を返す。
+// トークナイズと一緒にやる
+Token *consume(int expct_kind) {
+    Token* tk = calloc(1, sizeof(Token));
+    while (isspace(*src_p)){
+        src_p++;
+    }
+    //一文字演算子
+    if (strchr("+-*/()", *src_p)){
+        fprintf(stderr,"TK_RESERVED[%c]\n",*src_p);
+        tk->kind = *src_p;
+        tk->str = src_p;
+        tk->len = 1;
+        if(expct_kind == tk->kind){
+            fprintf(stderr,"\x1b[32mmatch(TK_RESERVED)\x1b[39m [%c]\n",expct_kind);
+            src_p++;
+            return tk;
+        }else{
+            fprintf(stderr,"Unmatch(TK_RESERVED) [%d != %d]%c\n",expct_kind, tk->kind,*src_p);
+            return NULL;
+        }
+    }
+    if (isdigit(*src_p)) {
+        tk->str = src_p;
+        int x = strtol(src_p, &src_p, 10);
+        tk->kind = TK_NUM;
+        tk->len = src_p - tk->str;
+        tk->val = x;
+        fprintf(stderr,"TK_NUM[%d] len:%d\n",x,tk->len);
+        if(expct_kind == TK_NUM){
+            fprintf(stderr,"\x1b[32mmatch(TK_NUM)\x1b[39m [%d]\n",tk->val);
+            return tk;
+        }else{
+            fprintf(stderr,"Unmatch(TK_NUM) [%d != %d]%c\n",expct_kind, TK_NUM,*src_p);
+            src_p = tk->str;
+            return NULL;
+        }
+    }
+    error_at(src_p,"Tokenize error\n");
+    return NULL;
+}
+Token *expect(int expct_kind) {
+    Token *tok = consume(expct_kind);
+    if (tok == NULL) {
+        error_at(src_p,"Tokenize error\n");
+    }
     return tok;
 }
-
-// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
-// 真を返す。それ以外の場合には偽を返す。
-bool consume(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    return false;
-  token = token->next;
-  return true;
-}
-
-// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
-// それ以外の場合にはエラーを報告する。
-void expect(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
-    //error("'%c'ではありません", op);
-    error_at(token->str, "is not [%c]",op);
-  token = token->next;
-}
-
-// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
-// それ以外の場合にはエラーを報告する。
 int expect_number() {
-    if (token->kind != TK_NUM)
-        //error("数ではありません");
-        //fprintf(stderr,"is not number");
-        error_at(token->str, "is not number");
-    int val = token->val;
-    fprintf(stderr,"Num : [%d]",val);
-    token = token->next;
-    return val;
-}
-
-bool at_eof() {
-  return token->kind == TK_EOF;
-}
-
-// 入力文字列srcをトークナイズしてそれを返す
-Token *tokenize() {
-    char* p = src;
-    Token head;
-    head.next = NULL;
-    Token *cur = &head;
-    fprintf(stderr,"tokenize\n");
-    while(*p) {
-        if (isspace(*p)){
-            p++;
-            continue;
-        }
-        if (strchr("+-*/()", *p)){
-            cur = new_token(TK_RESERVED, cur, p);
-            fprintf(stderr,"TK_RESERVED[%c]\n",*p);
-            p++;
-            continue;
-        }
-        if (isdigit(*p)) {
-            cur = new_token(TK_NUM, cur, p);
-            int x = 0;
-            x = strtol(p, &p, 10);
-            cur->val = x;
-            fprintf(stderr,"TK_NUM[%d]\n",x);
-            continue;
-        }
-        error_at(p,"Tokenize error\n");
+    fprintf(stderr,"TK_NUM [%d]\n",TK_NUM);
+    Token *tok = consume(TK_NUM);
+    if (tok == NULL) {
+        error_at(src_p,"Tokenize error\n");
     }
-
-    new_token(TK_EOF, cur, NULL);
-    return head.next;
+    int val = tok->val;
+    return val;
 }
 
 // ノード関連
@@ -142,58 +121,57 @@ struct Node {
 };
 // 二項演算子
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = kind;
-  node->lhs = lhs;
-  node->rhs = rhs;
-  return node;
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
 }
 // 数値
 Node *new_node_num(int val) {
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = ND_NUM;
-  node->val = val;
-  return node;
+    Node *node = new_node(ND_NUM,NULL,NULL);
+    node->val = val;
+    return node;
 }
 
 Node *expr();
 Node *mul();
 Node *primary();
 
-Node *expr(){
+Node *expr() {
     fprintf(stderr,"->expr()");
     Node *node = mul();
     while(1){
         if(consume('+')){
             node = new_node(ND_ADD,node,mul());
-            fprintf(stderr,"[+]");
+            fprintf(stderr,"\x1b[33m[+]\x1b[39m\n");
         }
         else if(consume('-')){//SubはAddの仲間
             node = new_node(ND_SUB,node,mul());
-            fprintf(stderr,"[-]");
+            fprintf(stderr,"\x1b[33m[-]\x1b[39m\n");
         }else{
             fprintf(stderr,"->expr()end\n");
             return node;
         }
     }
 }
-Node *mul(){
+Node *mul() {
     fprintf(stderr,"->mul()");
     Node *node = primary();
     while(1){
         if(consume('*')){
             node = new_node(ND_MUL,node,primary());
-            fprintf(stderr,"[*]");
+            fprintf(stderr,"\x1b[33m[*]\x1b[39m\n");
         }else if(consume('/')){
             node = new_node(ND_DIV,node,primary());
-            fprintf(stderr,"[/]");
+            fprintf(stderr,"\x1b[33m[/]\x1b[39m\n");
         }else{
             fprintf(stderr,"->mul()end\n");
             return node;
         }
     }
 }
-Node *primary(){
+Node *primary() {
     fprintf(stderr,"->primary()");
     if(consume('(')){
         fprintf(stderr,"->Bracket [(]");
@@ -203,26 +181,22 @@ Node *primary(){
         return node;
     }
     fprintf(stderr,"->primary() end\n");
-    return new_node_num(expect_number());
+    Node* node = new_node_num(expect_number());
+    fprintf(stderr,"\x1b[33m[%d]\x1b[39m\n",node->val);
+    return node;
     
 }
 
-void gen(){
+void gen() {
 }
 
 int main(int argc, char **argv){
     FILE *input_file = stdin;
     FILE *output_file = stdout;
     src = malloc(MAX_LINE * MAX_ONELINE);
+    src_p = src;
     size_t src_len = fread(src, 1, MAX_LINE * MAX_ONELINE - 1, input_file);
     src[src_len] = '\0';
-    token = tokenize();
-    Token *print_tkn = token;
-    while(1){
-        if(print_tkn->kind==TK_EOF)break;
-        if(print_tkn->kind==TK_RESERVED)fprintf(stderr,"kind:TK_RESERVED,str:%c\n",*print_tkn->str);
-        if(print_tkn->kind==TK_NUM)fprintf(stderr,"kind:TK_NUM,val:%d\n",print_tkn->val);
-        print_tkn = print_tkn->next;
-    }
     Node *node = expr();
+    gen();
 }
