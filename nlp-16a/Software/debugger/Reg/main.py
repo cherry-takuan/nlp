24dcb025722ecc1ac3541554053e310f049a2617
cherry <cherry@cherry-takuan.org>
2024-08-14 19:47:55 +0900

    Regのテスト

--- nlp-16a/Software/debugger/Reg/main.py
+++ nlp-16a/Software/debugger/Reg/main.py
@@ -6,7 +6,7 @@ A_PATTERN = [
         0x1234,
         0x5555,
         0xAAAA,
-        0xfFFF,
+        0xFFFF,
     ]
 def clk(id,address):
     phi1 = 1
@@ -37,37 +37,19 @@ if __name__ == '__main__':
     ALU_CTRL = 0x02
     IO_BUS = 0x03
 
-    print("A BUS CTRL DIR_SET")
     debug.dir_set(A_BUS_CTRL,0xFFFF)
-    print("Y BUS CTRL DIR_SET")
     debug.dir_set(Y_BUS_CTRL,0xFFFF)
-    print("ALU CTRL DIR_SET")
     debug.dir_set(ALU_CTRL,0x0FFF)
-    print("I/O BUS DIR_SET")
     debug.dir_set(IO_BUS,0x0000)
     debug.execute(IO_BUS,"REVERSE_MODE",0x0001)
 
-    debug.device_status(A_BUS_CTRL)
-    debug.device_status(Y_BUS_CTRL)
-    debug.device_status(ALU_CTRL)
-    debug.device_status(IO_BUS)
-
     IO_ADDRESS = 11
-    failed = 0
-    pass_num = 0
-    failed_list = list()
     phi1 = 0
     phi2 = 0
     debug.output(Y_BUS_CTRL,(0x0F<<10) | 0b1111111111 | phi1<<15 | phi2<<14)    #出力先を適当に設定
     debug.dir_set(IO_BUS,0x0000)
     debug.output(A_BUS_CTRL,(0x0F<<10) | 0b1111111111)                    #入力をIOに設定
-    #debug.output(ALU_CTRL,0b011111110110)#ALU転送命令
     debug.output(ALU_CTRL,0b011101001001)#ALU転送命令
     for A in range(0x10):
-        #debug.output(IO_BUS,A)
         clk(Y_BUS_CTRL,A)
-        sleep(1)
-        
-    if failed == 0:
-        print("test all passed")
     del debug
